bb08d7a4812c05d7a00217d3554214d9517328a0
Rajesh Kumar Bandaru <54278302+rkumar70900@users.noreply.github.com>
2024-12-22 22:38:59 -0600

    https://airtable.com/app0Yhnl3UFFh5Utn/tbl3jPJnVBCOdHgtM/viwNoGWlheq8W5MR7/rec0P1RhgfR4RZZhB?blocks=hide
    a random choice of race, class and background are being selected from data present in mongodb

--- inference_pipeline/dep.py
+++ inference_pipeline/dep.py
@@ -1,4 +1,5 @@
 import random
+from pymongo import MongoClient, errors
 
 class dependencies():
     def __init__(self):
@@ -13,3 +14,24 @@ class dependencies():
         rolls.remove(min(rolls))
         total = sum(rolls)
         return total
+    
+    def connect_mongo(self):
+        try:
+            # Attempt connection
+            client = MongoClient("mongodb://localhost:27017/", serverSelectionTimeoutMS=5000)
+            # Force connection check
+            client.admin.command("ping")
+            print("Connected to MongoDB!")
+            return client
+        except errors.ServerSelectionTimeoutError as err:
+            print("Error: Could not connect to MongoDB")
+            print(err)
+    
+    def get_context_collection(self, client, collection_name):
+        db = client["context_data"]
+        collection = db[collection_name]
+        return collection
+    
+    def get_distinct_names(self, collection):
+        distinct_names = collection.distinct("name")
+        return distinct_names
